07314cb92a94adcce123e194da387962457e0a10
yoshuaalexrull-a11y <yoshua.alex.rull@gmail.com>
2026-09-12 18:33:42 -0500

    subida 3

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -2,27 +2,18 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
-/**
- * @title Storage
- * @dev Store & retrieve value in a variable
- * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
- */
+/*
+    Primer contrato
+    JRU
+*/
 contract Storage {
 
-    uint256 number;
+    uint256 number; // uint256 = uint
 
-    /**
-     * @dev Store value in variable
-     * @param num value to store
-     */
     function store(uint256 num) public {
         number = num;
     }
 
-    /**
-     * @dev Return value 
-     * @return value of 'number'
-     */
     function retrieve() public view returns (uint256){
         return number;
     }
